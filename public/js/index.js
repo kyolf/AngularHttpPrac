59c5b5109718e68edbf41ac6cd64a5418653732a
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 17:40:13 -0800

    got post to database to work but it doesnt show up in the view until update

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,29 @@
 let comment_app = angular.module('comment_app', []);
 
-comment_app.service('commentObj', function($http) {
+comment_app.service('commentObj', function($http, $q) {
   this.comments = [];
 
   this.getComments = function() {
     return $http.get('/api/comment')
             .then(res=>{
               console.log(res);
-              if(res.statusText !== 'OK' && res.status !== '200'){
-                return Promise.reject(res.statusText);
+              if(res.statusText !== 'OK' || res.status !== 200) {
+                return $q.reject(res.statusText);
               }
               return res.data;
             });
   };
+
+  this.postComments = function(postObj) {
+    return $http.post('/api/comment', postObj)
+            .then(function(res) {
+              if(res.statusText !== 'OK' || res.status !== 201) {
+                return $q.reject(res.statusText);
+              }
+              return res.data;
+            });
+  };
+
 });
 
 comment_app.controller('CommentController', function($scope, commentObj) {
@@ -21,12 +32,20 @@ comment_app.controller('CommentController', function($scope, commentObj) {
     .getComments()
     .then(function(result) {
       $scope.comments = result;
+      console.log($scope.comments)
     });
   $scope.addComments = function() {
-    $scope.comments.push({
+    const postObj = {
       text: $scope.newText,
       likes: 0
-    });
+    };
+
+    commentObj
+      .postComments(postObj)
+      .then(function(result) {
+        $scope.comments.push(result);
+      });
+
     $scope.newText = '';
   };
 
